a9e067d2ef07dcf2e9b6a84ae0b27213c37211a8
Abdul Hafiz Ramadan <abdulhafizr>
2022-01-29 14:31:52 +0700

    Create module feature-calon-pemilik

---
+++ feature-calon-pemilik/src/main/java/com/ridhoafnidev/project/feature/calon/pemilik/CalonPemilikActivity.kt
@@ -0,0 +1,11 @@
+package com.ridhoafnidev.project.feature.calon.pemilik
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CalonPemilikActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calon_pemilik)
+    }
+}
