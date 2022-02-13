6a63cd524ab630a8763469417798f5329cfec6a9
Abdul Hafiz Ramadan <abdulhafizr>
2022-02-13 14:32:39 +0700

    Simulasi KPR

--- feature-detail-perumahan/src/main/java/com/ridhoafnidev/project/feature/detail_perumahan/SimulasiKPRActivity.kt
+++ feature-detail-perumahan/src/main/java/com/ridhoafnidev/project/feature/detail_perumahan/SimulasiKPRActivity.kt
@@ -12,11 +12,15 @@ import java.util.*
 class SimulasiKPRActivity : BaseActivity<ActivitySimulasiKprActivityBinding>(ActivitySimulasiKprActivityBinding::inflate) {
 
     private val arguments: SimulasiKPRActivityArgs by navArgs()
+    private val hargaProperti: Int
+        get() = arguments.hargaProperti
+    private val jumlahPinjaman: BigDecimal
+        get() = hargaProperti.toPercent()
 
     private val listJangkaWaktuPinjaman = (1..30).toList()
     private val listSukuBunga = listOf(
         6.00, 6.68, 7.70, 7.75, 8.00, 9.25, 9.75, 9.99, 10.29, 10.99, 11.29
-    ).toSukuBunganFormat()
+    )
 
     override fun initView() {
 
@@ -24,28 +28,45 @@ class SimulasiKPRActivity : BaseActivity<ActivitySimulasiKprActivityBinding>(Act
         initToolbar(back = true, primary = true)
         setPageName(getString(R.string.title_simulasi_kpr))
 
-        setFormSimulasiKPR(arguments.hargaProperti)
+        setFormSimulasiKPR(hargaProperti)
         setupEdtJangkaWaktuPinjaman(listJangkaWaktuPinjaman)
         setupEdtSukuBunga(listSukuBunga)
     }
 
     override fun initListener() {
+        binding.btnSimulasi.setOnClickListener {
+            val sukuBunga = binding.edtSukuBunga.text.toString().dropLast(1).toDouble()
+            val defaultJangkaWaktuPinjaman = binding.edtJangkaWaktuPinjaman.text.toString().toInt()
 
+            val angsuranPerBulan = jumlahPinjaman
+                .toAngsuranPerbulan(
+                    sukuBunga,
+                    defaultJangkaWaktuPinjaman
+                )
+
+            binding.tvAnguranPerbulan.text = angsuranPerBulan.toRupiah()
+        }
     }
 
     private fun setFormSimulasiKPR(hargaProperti: Int) {
         val jumlahDP = hargaProperti.toPercent(20.0)
-        val jumlahPinjaman = hargaProperti.toPercent(80.0)
+        val jumlahPinjaman = hargaProperti.toPercent()
         val defaultSukuBunga = listSukuBunga[0]
-//        val angsuranPerBulan = (jumlahPinjaman * defaultSukuBunga.toBigDecimal()) * (listJangkaWaktuPinjaman[0] / 120).toBigDecimal()
+        val defaultSukuBungaFormated = "${sukuBungaFormat.format(defaultSukuBunga)}%"
+        val defaultJangkaWaktuPinjaman = listJangkaWaktuPinjaman[0]
+        val angsuranPerBulan = jumlahPinjaman
+            .toAngsuranPerbulan(
+                defaultSukuBunga,
+                defaultJangkaWaktuPinjaman
+            )
 
         binding.apply {
             edtHargaProperty.setText(hargaProperti.toRupiah())
             edtUangMuka.setText(jumlahDP.toRupiah())
-            edtJangkaWaktuPinjaman.setText(listJangkaWaktuPinjaman[0].toString())
-            edtSukuBunga.setText(defaultSukuBunga)
+            edtJangkaWaktuPinjaman.setText(defaultJangkaWaktuPinjaman.toString())
+            edtSukuBunga.setText(defaultSukuBungaFormated)
             edtJumlahPinjaman.setText(jumlahPinjaman.toRupiah())
-//            tvAnguranPerbulan.text = angsuranPerBulan.toRupiah()
+            tvAnguranPerbulan.text = angsuranPerBulan.toRupiah()
         }
     }
 
@@ -54,12 +75,23 @@ class SimulasiKPRActivity : BaseActivity<ActivitySimulasiKprActivityBinding>(Act
         binding.edtJangkaWaktuPinjaman.setAdapter(arrayAdapter)
     }
 
-    private fun setupEdtSukuBunga(listSukuBunga: List<String>) {
-        val arrayAdapter = ArrayAdapter(this, R.layout.item_simulasi_kpr, R.id.tv_item_simulasi_kpr, listSukuBunga)
+    private fun setupEdtSukuBunga(listSukuBunga: List<Double>) {
+        val arrayAdapter = ArrayAdapter(
+            this,
+            R.layout.item_simulasi_kpr,
+            R.id.tv_item_simulasi_kpr,
+            listSukuBunga.toSukuBunganFormat()
+        )
         binding.edtSukuBunga.setAdapter(arrayAdapter)
     }
 
-    private fun Int.toPercent(percent: Double): BigDecimal =
+    private fun BigDecimal.toAngsuranPerbulan(sukuBunga: Double, tenor: Int): BigDecimal {
+        val cicilanPerhari = BigDecimal(this.toDouble() * (sukuBunga / 100)) / BigDecimal(tenor * 12)
+        val cicilanPerbulan = (cicilanPerhari * BigDecimal(24)) - BigDecimal(950_140)
+        return cicilanPerbulan
+    }
+
+    private fun Int.toPercent(percent: Double = 80.0): BigDecimal =
         BigDecimal((percent / 100) * toDouble())
 
     private fun List<Double>.toSukuBunganFormat(): List<String> =
